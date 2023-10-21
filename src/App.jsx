f1660fde4035b52209844b6fde87628cfa024c86
ajmir khan <ajmirk01@gmail.com>
2023-10-21 13:31:35 +0530

    all file first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className=" m-auto bg-pink-500 text-center text-3xl font-bold underline">
+      Ajmir Khan
+    </h1>
+    </>
+  )
+}
+
+export default App
+ 
